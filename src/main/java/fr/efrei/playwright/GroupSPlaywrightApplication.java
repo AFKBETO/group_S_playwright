b43bc34066c424d04dc900336e4e44020cbd5039
QV NGUYEN <qv.nguyen00@gmail.com>
2024-01-24 15:29:11 +0100

    init maven project

---
+++ src/main/java/fr/efrei/playwright/GroupSPlaywrightApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.playwright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupSPlaywrightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupSPlaywrightApplication.class, args);
+    }
+
+}
