97d803a02ab24c429df4a67d3f08ec378335dff7
Rimberse <kerimpltm@gmail.com>
2024-01-24 16:22:40 +0100

    Create first test to test out presence of elements

--- src/test/java/fr/efrei/playwright/GroupSPlaywrightApplicationTests.java
+++ src/test/java/fr/efrei/playwright/GroupSPlaywrightApplicationTests.java
@@ -1,13 +1,35 @@
 package fr.efrei.playwright;
 
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import java.util.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @SpringBootTest
 class GroupSPlaywrightApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void ensureElementsAreVisible() {
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType chromium = playwright.chromium();
+            Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = browser.newPage();
+            page.navigate("https://s.hr.dmerej.info");
+
+            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List Employees"))).isVisible();
+            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Add new employee"))).isVisible();
+            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("List teams"))).isVisible();
+            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Create new team"))).isVisible();
+            assertThat(page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Reset database"))).isVisible();
+            browser.close();
+        }
     }
 
 }
